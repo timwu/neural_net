d2b2425719f01405085e252c5114d9f42004db8a
Tim Wu <tim@urx.com>
2015-08-20 21:03:36 -0700

    plot costs with seaborn

--- neural_net.py
+++ neural_net.py
@@ -5,7 +5,10 @@ import numpy as np
 import theano.tensor as T
 import theano
 import sklearn.metrics
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+plt.ioff()
 
 class HiddenLayer(object):
     def __init__(self, n_in, n_out):
@@ -114,13 +117,14 @@ def main():
     NLL = -T.mean(T.log(mlp_output)[T.arange(y.shape[0]), y])
     cost = NLL + 0.01 * mlp.l2
 
-    alpha = 0.2
+    alpha = 0.1
     updates = [(param, param - alpha * T.grad(cost, param)) for param in mlp.params]
 
     train_mlp = theano.function(inputs=[x, y], outputs=cost, updates=updates)
     predict = theano.function(inputs=[x], outputs=mlp_predictions)
 
     prev_cost = np.inf
+    costs = []
     for i in range(300):
         cur_cost = train_mlp(train_set[0], train_set[1].astype('int32'))
         print cur_cost
@@ -128,11 +132,15 @@ def main():
         #     print "Gave up after %i iterations" % i
         #     break
         prev_cost = cur_cost
+        costs.append(cur_cost)
 
     predictions = predict(test_set[0])
 
     print(sklearn.metrics.confusion_matrix(test_set[1], predictions))
     print(sklearn.metrics.classification_report(test_set[1], predictions))
+
+    sns.tsplot(costs)
+    plt.show()
 
 if __name__ == "__main__":
     main()
