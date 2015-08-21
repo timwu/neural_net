4f85b9ed8d111622ed932ce3b63ad414e586872e
Tim Wu <tim@urx.com>
2015-08-20 17:31:34 -0700

    make it actually work

--- neural_net.py
+++ neural_net.py
@@ -9,23 +9,23 @@ import sklearn.metrics
 
 class HiddenLayer(object):
     def __init__(self, n_in, n_out):
-        rng = np.random.RandomState(123984)
+        rng = np.random.RandomState()
 
         self.shape = (n_in, n_out)
 
         self.W = theano.shared(rng.uniform(low=-np.sqrt(6. / (n_in + n_out)),
                                            high=np.sqrt(6. / (n_in + n_out)),
                                            size=self.shape).astype(theano.config.floatX),
-                               name='W',
+                               name='Wh',
                                borrow=True)
 
         self.b = theano.shared(np.zeros((n_out,), dtype=theano.config.floatX),
-                               name='b', borrow=True)
+                               name='bh', borrow=True)
 
         self.params = [self.W, self.b]
 
     def output(self, input):
-        return T.tanh(T.dot(input, self.W) + self.b)
+        return T.maximum(0, T.dot(input, self.W) + self.b)
 
     def connect(self, next_layer):
         assert self.shape[1] == next_layer.shape[0]
@@ -34,17 +34,17 @@ class HiddenLayer(object):
 
     @property
     def l2(self):
-        return T.mean(self.W ** 2)
+        return T.mean(T.power(self.W, 2))
 
 
 class LogisticLayer(object):
     def __init__(self, n_in, n_out):
         self.shape = (n_in, n_out)
         self.W = theano.shared(np.zeros(self.shape, dtype=theano.config.floatX),
-                               name='W', borrow=True)
+                               name='Wl', borrow=True)
 
         self.b = theano.shared(np.zeros((n_out,), dtype=theano.config.floatX),
-                               name='b', borrow=True)
+                               name='bl', borrow=True)
 
         self.params = [self.W, self.b]
 
@@ -57,7 +57,7 @@ class LogisticLayer(object):
 
     @property
     def l2(self):
-        return T.mean(self.W ** 2)
+        return T.mean(T.power(self.W, 2))
 
 
 class MLP(object):
@@ -65,7 +65,10 @@ class MLP(object):
         self.layers = [in_layer]
 
     def output(self, input):
-        return self.out_layer.output(self.in_layer.output(input))
+        output = input
+        for layer in self.layers:
+            output = layer.output(output)
+        return output
 
     @property
     def shape(self):
@@ -88,11 +91,7 @@ class MLP(object):
 
     @property
     def l2(self):
-        return (self.in_layer.l2 + self.out_layer.l2) / 2.0
-        # l2 = self.in_layer.l2
-        # for layer in self.layers[1:]:
-        #     l2 += layer.l2
-        # return l2
+        return reduce(lambda x, y: x + y, [layer.l2 for layer in self.layers]) / len(self.layers)
 
     def connect(self, next_layer):
         assert next_layer.shape[0] == self.out_layer.shape[1]
@@ -107,7 +106,7 @@ def main():
     x = T.matrix('x')
     y = T.ivector('y')
 
-    mlp = HiddenLayer(784, 32).connect(HiddenLayer(32, 64)).connect(LogisticLayer(64, 10))
+    mlp = HiddenLayer(784, 64).connect(HiddenLayer(64, 64)).connect(HiddenLayer(64, 32)).connect(LogisticLayer(32, 10))
     mlp_output = mlp.output(x)
     mlp_predictions = T.argmax(mlp_output, axis=1)
 
@@ -124,7 +123,7 @@ def main():
     for i in range(1000):
         cur_cost = train_mlp(train_set[0], train_set[1].astype('int32'))
         print cur_cost
-        if abs((cur_cost - prev_cost) / prev_cost) < 0.001:
+        if i > 50 and abs((cur_cost - prev_cost) / prev_cost) < 0.001:
             print "Gave up after %i iterations" % i
             break
         prev_cost = cur_cost
